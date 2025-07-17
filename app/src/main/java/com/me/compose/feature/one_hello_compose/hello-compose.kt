82f083863eab56e91ec8dac145e7a05805a304b2
azadbasis <azadbasis@gmail.com>
2025-07-18 00:23:56 +0600

    Column use for Combine multiple Text components vertically

--- app/src/main/java/com/me/compose/feature/one_hello_compose/hello-compose.kt
+++ app/src/main/java/com/me/compose/feature/one_hello_compose/hello-compose.kt
@@ -1,6 +1,7 @@
 package com.me.compose.feature.one_hello_compose
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -8,25 +9,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
+@Preview
 @Composable
-fun CreateText(name: String) {
+fun CreateText(name: String = "World") {
     Box (modifier = Modifier.padding(30.dp), contentAlignment = Alignment.Center){
-        Text(
+        Column {
+            Text(
+                "Assalaamu Alaikum $name",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    color = MaterialTheme.colorScheme.tertiary,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    letterSpacing = 2.sp
+                ),
 
-            "Assalaamu Alaikum $name",
-            style = MaterialTheme.typography.bodyLarge.copy(
-                color = MaterialTheme.colorScheme.tertiary,
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp,
-                letterSpacing = 2.sp
-            ),
-            color = MaterialTheme.colorScheme.tertiary,
-
-        )
+                )
+            Text(
+                "Welcome to the world of Compose $name",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    color = MaterialTheme.colorScheme.primary,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    letterSpacing = 2.sp
+                ),
+                textAlign = TextAlign.Center
+            )
+        }
     }
 
 }
