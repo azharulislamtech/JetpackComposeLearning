e1e02905f2612080fdd0831b7183d307946d739b
azadbasis <azadbasis@gmail.com>
2025-07-18 17:22:47 +0600

    Text with border, ripple effect

--- app/src/main/java/com/me/compose/feature/one_hello_compose/hello-compose.kt
+++ app/src/main/java/com/me/compose/feature/one_hello_compose/hello-compose.kt
@@ -1,45 +1,84 @@
 package com.me.compose.feature.one_hello_compose
 
+import android.view.Surface
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.nio.file.WatchEvent
 
 
 @Preview
 @Composable
 fun CreateText(name: String = "World") {
-    Box (modifier = Modifier.padding(30.dp), contentAlignment = Alignment.Center){
-        Column {
-            Text(
-                "Assalaamu Alaikum $name",
-                style = MaterialTheme.typography.bodyLarge.copy(
-                    color = MaterialTheme.colorScheme.tertiary,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp,
-                    letterSpacing = 2.sp
-                ),
-
-                )
+    Column(
+        modifier = Modifier.padding(30.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Assalaamu Alaikum $name",
+            style = MaterialTheme.typography.bodyLarge.copy(
+                color = MaterialTheme.colorScheme.tertiary,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                letterSpacing = 2.sp
+            ),
+            textAlign = TextAlign.Center,
+        )
+        Box(
+            modifier = Modifier
+                .padding(top = 25.dp)
+                .border(1.dp, Color.Black, RoundedCornerShape(16.dp))
+                .clip(RoundedCornerShape(16.dp))
+                .background(MaterialTheme.colorScheme.primary),
+            contentAlignment = Alignment.Center
+        ) {
             Text(
                 "Welcome to the world of Compose $name",
-                style = MaterialTheme.typography.bodyLarge.copy(
-                    color = MaterialTheme.colorScheme.primary,
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 20.sp,
-                    letterSpacing = 2.sp
-                ),
-                textAlign = TextAlign.Center
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(10.dp)
             )
+        }
+        //
+
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            color = MaterialTheme.colorScheme.secondary,
+            border = BorderStroke(1.dp, Color.Black),
+            modifier = Modifier.padding(top = 10.dp),
+            onClick = { /* Your click action */ }, // Surface is clickable by default when onClick is provided!
+        ) {
+
+            Text(
+                "Text use as a card",
+                modifier = Modifier
+                    .padding(10.dp),
+                color = Color.White,
+                textAlign = TextAlign.Center,
+
+
+                )
         }
     }
 
