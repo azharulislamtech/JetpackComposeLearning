d67578d66285c0a8ae62cb4a5ebbdddcbee55926
azadbasis <azadbasis@gmail.com>
2025-07-23 13:01:55 +0600

    Layout Components in Composable practices DONE!

--- app/src/main/java/com/me/compose/feature/two_layout_lab/presentation/pegination/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/me/compose/feature/two_layout_lab/presentation/pegination/viewmodel/UserViewModel.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-// UserViewModel.kt
+
 class UserViewModel(
     private val repository: UserRepository
 ) : ViewModel() {
